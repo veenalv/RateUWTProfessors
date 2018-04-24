9c20a7e278540d71aec5d18f41abedf8b2dbfd09
Maksim VOznyarskyi <maksimv@uw.edu>
2018-04-24 11:03:58 -0700

    added my email

--- app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+// maksimv@uw.edu
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
