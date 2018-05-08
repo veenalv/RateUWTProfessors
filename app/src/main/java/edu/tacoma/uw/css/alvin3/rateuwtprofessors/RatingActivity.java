0140fd9e3e379a367a7b7ef91c19f6260c35c551
alveeno <nguyenalvind@gmail.com>
2018-05-08 16:04:19 -0700

    search icon in appbar has shown up.

--- app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/RatingActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/RatingActivity.java
@@ -3,6 +3,9 @@ package edu.tacoma.uw.css.alvin3.rateuwtprofessors;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import edu.tacoma.uw.css.alvin3.rateuwtprofessors.rating.Rating;
 
@@ -22,6 +25,29 @@ public class RatingActivity extends AppCompatActivity implements
         }
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_rating, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            Toast.makeText(this, "Showing courses in CSS", Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
