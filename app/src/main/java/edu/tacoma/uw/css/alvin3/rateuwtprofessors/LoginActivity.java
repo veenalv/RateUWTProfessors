4d7adb64f30b561162f24084305f39f7f58a4f7e
Maksim VOznyarskyi <maksimv@uw.edu>
2018-05-07 17:59:14 -0700

    toast is displayed if username or password are wrong.

--- app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/LoginActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/LoginActivity.java
@@ -60,7 +60,7 @@ public class LoginActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("login", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(getBaseContext(), "Authentication failed.",
+                            Toast.makeText(getBaseContext(), "Invalid username or password",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
