3b4583c155057f312a909a77cbf6a7b3ff475e45
alveeno <nguyenalvind@gmail.com>
2018-05-10 17:07:01 -0700

    correct app bar added

--- app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/RatingActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/RatingActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,6 +32,8 @@ public class RatingActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         rlf = new RatingListFragment();
         if (findViewById(R.id.rating_fragment_container)!= null) {
             getSupportFragmentManager().beginTransaction()
