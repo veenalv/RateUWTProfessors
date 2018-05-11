50a4c7786eddd93a288ccd3bff1d69ff1689c4ea
Maksim VOznyarskyi <maksimv@uw.edu>
2018-05-10 23:35:09 -0700

    added java doc to registration activity

--- app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/RegisterActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/RegisterActivity.java
@@ -1,3 +1,6 @@
+/**
+ * TCSS 450 - Spring 2018 Team 8. Alvin, Hui, and Maksim.
+ */
 package edu.tacoma.uw.css.alvin3.rateuwtprofessors;
 
 import android.content.Context;
@@ -5,7 +8,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.nfc.Tag;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
@@ -24,6 +26,17 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 
+/**
+ * RegisterActivity is the activity that is used when a user wants to register an account.
+ * In order to register an account, the app must be connected to the internet, the email must
+ * end with '@uw.edu', the password must be of length 6 - 15, and the checkbox must be enabled.
+ * After a successful registration, a toast will be shown that mentions that an email verification
+ * has been sent. The email verification must be completed before the user can sign in.
+ *
+ * @author Maksim
+ * @auther Alvin
+ * @auther Hui
+ */
 public class RegisterActivity extends AppCompatActivity {
     /** The email used to register an account. */
     private EditText mEmail;
@@ -39,82 +52,108 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // Initialize all the variables.
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
         mCheckBox = (CheckBox) findViewById(R.id.checkBox);
         mAuth = FirebaseAuth.getInstance();
     }
 
+    /**
+     * Method that is used to try registering an account.
+     * @param view
+     */
     protected void register(View view) {
+        // Logs for debugging
         Log.d("Register: ", mEmail.getText().toString());
         Log.d("Register: ", mPassword.getText().toString());
         Log.d("Register: ", Boolean.toString(mCheckBox.isChecked()));
 
-        // register now
-        if (!mCheckBox.isChecked()) {
-            Toast.makeText(getBaseContext(),"Must agree to terms in order to register",
-                    Toast.LENGTH_SHORT).show();
+        // Section that handles registration
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        // If not connected to internet, display dialog.
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            notConnectedToInternet();
+        // Make sure that checkbox is checked.
+        } else if (!mCheckBox.isChecked()) {
+            Toast.makeText(getBaseContext(),"Must agree to terms in order to register", Toast.LENGTH_SHORT).show();
+        // Make sure that a @uw.edu email is used.
+        } else if (!mEmail.getText().toString().endsWith("@uw.edu")) {
+            Toast.makeText(getBaseContext(), "must use uw.edu email", Toast.LENGTH_SHORT).show();
+        // Make sure that password has a min length of 6 and max length of 15.
+        } else if (mPassword.getText().toString().length() < 6 || mPassword.getText().toString().length() > 15) {
+            Toast.makeText(this, "Password must be 6 to 15 digits long", Toast.LENGTH_SHORT).show();
         } else {
-            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-            if (networkInfo == null || !networkInfo.isConnected()) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-                builder.setMessage("Must be connected to the internet").setTitle("Error");
-                builder.setNeutralButton("Open WIFI settings", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User clicked OK button
-                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
-                    }
-                });
-                builder.setPositiveButton("close", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User clicked OK button
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            } else if (!mEmail.getText().toString().endsWith("@uw.edu")) {
-                Toast.makeText(getBaseContext(), "must use uw.edu email", Toast.LENGTH_SHORT).show();
-            } else if (mPassword.getText().toString().length() < 6 || mPassword.getText().toString().length() > 15) {
-                Toast.makeText(this, "Password must be 6 to 15 digits long", Toast.LENGTH_SHORT).show();
-            } else {
-                mAuth.createUserWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString())
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    user.sendEmailVerification()
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    if (task.isSuccessful()) {
-                                                        Toast.makeText(getBaseContext(), "Email verification sent",
-                                                                Toast.LENGTH_SHORT).show();
-                                                    }
+            // Try registering the account with given credentials.
+            mAuth.createUserWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Registration is successful.
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                // Send email verification to the email provided.
+                                user.sendEmailVerification()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(getBaseContext(), "Email verification sent",
+                                                            Toast.LENGTH_SHORT).show();
                                                 }
-                                            });
+                                            }
+                                        });
 
+                            } else {
+                                // If sign in fails, display a toast to the user.
+                                Log.w("Registration: ", "createUserWithEmail:failure", task.getException());
+                                // If account is already registered, notify the user that.
+                                if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                    Toast.makeText(getBaseContext(), "Account is already registered",
+                                            Toast.LENGTH_SHORT).show();
                                 } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("Registration: ", "createUserWithEmail:failure", task.getException());
-                                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
-                                        Toast.makeText(getBaseContext(), "Account is already registered",
-                                                Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        Toast.makeText(getBaseContext(), "Authentication failed.",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
+                                    // display generic fail toast.
+                                    Toast.makeText(getBaseContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
                                 }
                             }
-                        });
-            }
+                        }
+                    });
         }
     }
 
+    /**
+     *  Method is called to go back to the sign in activity.
+     *
+     * @param view is the view that is passed in.
+     */
     protected void finish(View view) {
         this.finish();
+    }
+
+    /**
+     * Private helper method that is used to display a dialog that warns the user
+     * that the app is not connected to the internet. THey can either close the
+     * dialog, or select the button to open the wifi settings.
+     */
+    private void notConnectedToInternet() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Must be connected to the internet").setTitle("Error");
+        builder.setNeutralButton("Open WIFI settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked 'Open WIFI settings' button
+                startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+            }
+        });
+        builder.setPositiveButton("close", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
