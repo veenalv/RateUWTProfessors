e469c4d994eabf7d9690f65c7e0f11b093c20cae
alveeno <nguyenalvind@gmail.com>
2018-05-27 11:55:01 -0700

    url generation complete in android code. now i need to work on the web service php files to populate our database.

--- app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/HomeActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/HomeActivity.java
@@ -15,6 +15,8 @@ package edu.tacoma.uw.css.alvin3.rateuwtprofessors;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -29,6 +31,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import edu.tacoma.uw.css.alvin3.rateuwtprofessors.professor.Professor;
 import edu.tacoma.uw.css.alvin3.rateuwtprofessors.professor.ProfessorDetail;
 
@@ -38,6 +49,7 @@ import edu.tacoma.uw.css.alvin3.rateuwtprofessors.professor.ProfessorDetail;
  */
 public class HomeActivity extends AppCompatActivity implements
         ProfessorListFragment.OnListFragmentInteractionListener,
+        RatingAddFragment.OnFragmentInteractionListener,
         ProfessorDetailFragment.OnListFragmentInteractionListener{
 
     /**
@@ -65,9 +77,11 @@ public class HomeActivity extends AppCompatActivity implements
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "test add button", Toast.LENGTH_SHORT)
-                        .show();
-
+                RatingAddFragment courseAddFragment = new RatingAddFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.rating_fragment_container, courseAddFragment)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
 
@@ -247,4 +261,86 @@ public class HomeActivity extends AppCompatActivity implements
         //handle user clicking on ProfessorDetail items here.
         //currently nothing here because we disabled clicking on items in ProfessorDetail.
     }
+
+    /**
+     * Over ridden methods from RatingAddFragment
+     */
+    @Override
+    public void addRating(String url) {
+
+        AddRatingTask task = new AddRatingTask();
+        task.execute(new String[]{url.toString()});
+
+        // Takes you back to the previous fragment by popping the current fragment out.
+        getSupportFragmentManager().popBackStackImmediate();
+    }
+
+
+    private class AddRatingTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to add Rating, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if (status.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "Rating successfully added!"
+                            , Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to add: "
+                                    + jsonObject.get("error")
+                            , Toast.LENGTH_LONG)
+                            .show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Something wrong with the data" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/RatingAddFragment.java
@@ -0,0 +1,182 @@
+package edu.tacoma.uw.css.alvin3.rateuwtprofessors;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URLEncoder;
+
+import static android.content.ContentValues.TAG;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RatingAddFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RatingAddFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RatingAddFragment extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private EditText mOverallQuality;
+    private EditText mDifficulty;
+    private EditText mTeachingAbility;
+    private EditText mHelpOffered;
+    private EditText mWrittenReview;
+
+    private OnFragmentInteractionListener mListener;
+
+    private final static String RATING_ADD_URL
+            = "http://tcssalvin.000webhostapp.com/android/addRating.php?";
+
+    public RatingAddFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RatingAddFragment.
+     */
+    public static RatingAddFragment newInstance(String param1, String param2) {
+        RatingAddFragment fragment = new RatingAddFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_rating_add, container, false);
+        mOverallQuality = (EditText) v.findViewById(R.id.overallquality);
+        mDifficulty = (EditText) v.findViewById(R.id.difficulty);
+        mTeachingAbility = (EditText) v.findViewById(R.id.teachingability);
+        mHelpOffered = (EditText) v.findViewById(R.id.helpoffered);
+        mWrittenReview = (EditText) v.findViewById(R.id.writtenreview);
+
+        //hide the floating action button
+        FloatingActionButton floatingActionButton = (FloatingActionButton)
+                getActivity().findViewById(R.id.fab);
+        floatingActionButton.hide();
+
+        //add a listener to the add button
+        Button addRatingButton = (Button) v.findViewById(R.id.btnCourse);
+        addRatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = buildCourseURL(v);
+                mListener.addRating(url);
+            }
+        });
+
+
+        return v;
+    }
+
+    private String buildCourseURL(View v) {
+
+        StringBuilder sb = new StringBuilder(RATING_ADD_URL);
+
+        try {
+
+            String quality = mOverallQuality.getText().toString();
+            sb.append("quality=");
+            sb.append(URLEncoder.encode(quality, "UTF-8"));
+
+
+            String difficulty = mDifficulty.getText().toString();
+            sb.append("&difficulty=");
+            sb.append(URLEncoder.encode(difficulty, "UTF-8"));
+
+
+            String teachingAbility = mTeachingAbility.getText().toString();
+            sb.append("&teaching=");
+            sb.append(URLEncoder.encode(teachingAbility, "UTF-8"));
+
+            String helpOffered = mHelpOffered.getText().toString();
+            sb.append("&help=");
+            sb.append(URLEncoder.encode(helpOffered, "UTF-8"));
+
+            String writtenReview = mWrittenReview.getText().toString();
+            sb.append("&review=");
+            sb.append(URLEncoder.encode(writtenReview, "UTF-8"));
+
+            Log.i("MY_URL", sb.toString());
+
+        }
+        catch(Exception e) {
+            Toast.makeText(v.getContext(), "Something wrong with the url" + e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+        return sb.toString();
+    }
+
+    public void onButtonPressed(String uri) {
+        if (mListener != null) {
+            mListener.addRating(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void addRating(String url);
+    }
+}
