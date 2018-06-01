8e8c517a1e954bb1da158af92717f283d29e94e9
Maksim VOznyarskyi <maksimv@uw.edu>
2018-05-31 18:49:05 -0700

    added java doc to share content dialog class

--- app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/ShareProfessorDialogFragment.java
+++ app/src/main/java/edu/tacoma/uw/css/alvin3/rateuwtprofessors/ShareProfessorDialogFragment.java
@@ -1,3 +1,9 @@
+/**
+ * TCSS 450 - Spring 2018 Team 8.
+ * @author Alvin Nguyen
+ * @author Maksim B Voznyarskiy
+ * @author Hui Ting Cai
+ */
 package edu.tacoma.uw.css.alvin3.rateuwtprofessors;
 
 import android.app.Dialog;
@@ -10,19 +16,41 @@ import android.support.v7.app.AlertDialog;
 
 import static java.net.Proxy.Type.HTTP;
 
+/**
+ * Fragment that is used to display the share content dialog.
+ */
 public class ShareProfessorDialogFragment extends android.support.v4.app.DialogFragment {
+    /** message that the will be shared. */
     private String mMessage;
 
+    /**
+     * Sets the content message.
+     *
+     * @param message is the message to set the mMessage to.
+     */
     public void setMessage(String message) {
         mMessage = message;
     }
 
+    /**
+     * Method called to create the share content dialog.
+     *
+     * @param savedInstanceState is the bundle.
+     *
+     * @return the dialog.
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("Share this rating!")
                 .setPositiveButton("Share", new DialogInterface.OnClickListener() {
+                    /**
+                     * On click listener for the share button.
+                     *
+                     * @param dialog is the dialog.
+                     * @param which is which button is pressed.
+                     */
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // share the rating
@@ -34,6 +62,12 @@ public class ShareProfessorDialogFragment extends android.support.v4.app.DialogF
                 })
 
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    /**
+                     * On click listener for the cancel button.
+                     *
+                     * @param dialog is the dialog.
+                     * @param which is which button is pressed.
+                     */
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // User cancelled the dialog
